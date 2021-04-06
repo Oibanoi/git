1de408bfa8b23112e6a1ea0e8def500a772928fc
huupb <huupbhe160316>
2021-04-06 23:07:04 +0700

    cap nhat footer

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Post(models.Model):
@@ -7,5 +7,11 @@ class Post(models.Model):
     body = models.TextField()
     date= models.DateTimeField(auto_now_add=True)
     image=models.ImageField(null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True,default=None, null=True)
     def __str__(self):
         return self.title
+class comment (models.Model):
+    post= models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comment')
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    body=models.TextField
+    date= models.DateTimeField(auto_now_add=True) 
