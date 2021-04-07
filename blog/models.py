a1a99388217b8b183edd595d9f16d218f942e07f
huupb <huupbhe160316>
2021-04-07 10:19:04 +0700

    thêm phần bình luận cho bài viết

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True,default=None, null=True)
     def __str__(self):
         return self.title
-class comment (models.Model):
-    post= models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comment')
+class Comment (models.Model):
+    post= models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    body=models.TextField
+    body=models.TextField()
     date= models.DateTimeField(auto_now_add=True) 
